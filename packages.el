5adeea70029333fb08ac8841be5c63222ced7f80
Marsel Scheer <scheer@freescience.de>
2020-09-24 20:53:28 +0200

    add poly-R to package list

--- packages.el
+++ packages.el
@@ -48,3 +48,8 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+'(package! poly-R
+   :recipe (:host github :repo "polymode/poly-R"))
+
+          
